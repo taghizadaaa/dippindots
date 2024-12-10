d6fc3ade1da28413e17d068f0fe0f25dd25dfdd9
taghizadaaa <araztagizadeh@gmail.com>
2024-12-10 21:59:51 +0400

    CartDrop open close items

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,31 +1,89 @@
-import React from 'react'
-import { Link } from 'react-router'
-import logo from "../assets/Images/logo-dippin-dots-official.png"
+import React, { useState } from 'react';
+import { Link } from 'react-router';
+import logo from "../assets/Images/logo-dippin-dots-official.png";
 import iceLeft from "../assets/Images/ice-cap-left.svg";
 import iceRight from "../assets/Images/ice-cap-right.svg";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 
-
 const Header = () => {
+  // State to track cart dropdown visibility
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  // Toggle cart dropdown visibility
+  const toggleCart = () => setIsCartOpen(!isCartOpen);
+
+  // Close cart dropdown
+  const closeCart = () => setIsCartOpen(false);
+
   return (
     <header>
+      {/* Modal Background */}
+      {isCartOpen && <div className="modalBackground" onClick={closeCart}></div>}
+      
       <div className="top">
+        {/* Account Link */}
         <div className="account">
-          <Link className='blueLink' to="/account"><FaRegUserCircle /> ACCOUNT</Link>
+          <Link className='blueLink' to="/account">
+            <FaRegUserCircle /> ACCOUNT
+          </Link>
         </div>
+        
+        {/* Cart Link */}
         <div className="cart">
-        <Link className='blueLink'><FaShoppingCart /> CART</Link>
+          <Link className='blueLink' onClick={toggleCart}>
+            <FaShoppingCart /> CART
+          </Link>
+        </div>
+        
+        {/* Cart Dropdown */}
+        {isCartOpen && (
+          <div className="cartDrop">
+            <h5>Your cart contains item</h5>
+            <div className="content">
+              <div className="checkout">
+                <h5 className="subtotal">
+                  Order subtotal <strong>Price</strong>
+                </h5>
+                <div className="buttons">
+                  <button>
+                    <Link to="/cart" className='classBlue'>
+                      View / Edit Cart
+                    </Link>
+                  </button>
+                </div>
+              </div>
+              <hr />
+              <ul className="products">
+                <li>
+                  <Link>
+                    <img src={logo} alt="" />
+                  </Link>
+                  <div className="cnt">
+                    <h3>Banana Split Ice Cream</h3>
+                    <p>Count vur Price</p>
+                    <div className="options">
+                      <p>Size: Bulk Bag</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* Ice Decorations */}
+      <div className="ice">
+        <div className="left">
+          <img src={iceLeft} alt="ice" />
+        </div>
+        <div className="right">
+          <img src={iceRight} alt="ice" />
         </div>
       </div>
-      <div className="ice">
-          <div className="left">
-            <img src={iceLeft} alt="ice" />
-          </div>
-          <div className="right">
-          <img src={iceRight} alt="ice" />
-          </div>
-        </div>
+
+      {/* Navigation */}
       <div className="container">
         <nav>
           <div className="logo">
@@ -40,11 +98,13 @@ const Header = () => {
             <li><Link className='whiteLink'>SELL</Link></li>
             <li><Link className='whiteLink'>DOT CRAZY! EMAIL CLUB</Link></li>
           </ul>
-          <div className="contact"><Link className='contactLink' to="/contact">CONTACT US</Link></div>
+          <div className="contact">
+            <Link className='contactLink' to="/contact">CONTACT US</Link>
+          </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
